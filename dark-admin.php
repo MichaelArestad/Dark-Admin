2c588cbd8d67945d6784b8262ba973158563b797
Egill Erlendsson <egill@automattic.com>
2015-08-29 17:39:42 +0000

    Helper function to replace/override css
    Created a helper function to simplify replacing the stylesheets in
    $wp_styles.

--- dark-admin.php
+++ dark-admin.php
@@ -17,87 +17,80 @@ class DarkAdmin {
 		if ( ! defined( 'DARKADMIN' ) ) {
 			define( 'DARKADMIN', true );
 		}
-		add_action( 'init', array( $this, 'register_open_sans' ) );
-		remove_action( 'admin_init', 'register_admin_color_schemes', 1 );
-		add_action( 'admin_init', array( $this, 'register_admin_color_scheme' ) );
-		add_action( 'login_init', array( $this, 'replace_wp_default_styles' ) );
-		add_action( 'login_init', array( $this, 'fix_login_color_scheme' ) );
-		add_action( 'init', array( $this, 'replace_admin_bar_style' ) );
-		add_action( 'admin_init', array( $this, 'replace_wp_default_styles' ) );
-		add_action( 'wp_head', array( $this, 'override_toolbar_margin' ), 11 );
+		remove_action( 'admin_init',               'register_admin_color_schemes', 1 );
+		add_action( 'admin_init',                  array( $this, 'register_admin_color_scheme' ) );
+		add_action( 'login_init',                  array( $this, 'replace_wp_default_styles' ) );
+		add_action( 'login_init',                  array( $this, 'fix_login_color_scheme' ) );
+		add_action( 'init',                        array( $this, 'replace_admin_bar_style' ) );
+		add_action( 'admin_init',                  array( $this, 'replace_wp_default_styles' ) );
+		add_filter( 'tiny_mce_before_init',        array( $this, 'mce_init' ) );
 
-		add_filter( 'tiny_mce_before_init', array( $this, 'mce_init' ) );
-		add_filter( 'style_loader_tag', array( $this, 'fix_style_tag_href' ) );
+		add_filter( 'style_loader_tag',            array( $this, 'fix_style_tag_href' ) );
 		add_filter( 'get_user_option_admin_color', array( $this, 'force_admin_color' ) );
-		add_filter( 'body_class', array( $this, 'add_body_class_frontend' ) );
-		add_filter( 'admin_body_class', array( $this, 'add_body_class_backend' ) );
+		add_filter( 'body_class',                  array( $this, 'add_body_class_frontend' ) );
+		add_filter( 'admin_body_class',            array( $this, 'add_body_class_backend' ) );
+
+		add_action( 'wp_head',                     array( $this, 'override_toolbar_margin' ), 11 );
 	}
 
-	public function register_open_sans() {
-		wp_register_style(
-			'open-sans',
-			'//fonts.googleapis.com/css?family=Open+Sans:400italic,600italic,400,600&subset=latin-ext,latin',
-			plugins_url( 'css/opensans.css', __FILE__ ),
-			false,
-			'20130605'
-		);
-	}
-
-	public function register_admin_color_scheme() {
-		global $wp_styles, $_wp_admin_css_colors;
-		wp_admin_css_color(
-			'darkadmin',
-			__( 'DarkAdmin' ),
-			plugins_url( 'css/colors-mp6.css', __FILE__ ),
-			array( '#222', '#333', '#0074a2', '#2ea2cc' )
-		);
-		$_wp_admin_css_colors['darkadmin']->icon_colors = array( 'base' => '#999', 'focus' => '#2ea2cc', 'current' => '#fff' );
-		// set modification time
-		$wp_styles->registered['colors']->ver    = filemtime( plugin_dir_path( __FILE__ ) . 'css/colors-mp6.css' );
-		// set dependencies
-		$wp_styles->registered['colors']->deps[] = 'open-sans';
-		$wp_styles->registered['colors']->deps[] = 'dashicons';
-	}
-
-	public function fix_login_color_scheme() {
+	/**
+	 * Helper function to replace enqueued stylesheets with a version in the plugin.
+	 *
+	 * Replaces or overrides the enqueued stylesheet in the $wp_styles global.
+	 *
+	 * @since x.x.x
+	 * @access (private)
+	 *
+	 * @global type $wp_styles Enqueued stylesheets.
+	 *
+	 * @param type $handle. Name of the stylesheet to override.
+	 * @param type $css_file Filename of the CSS used to replace (e.g. file.css).
+	 * @param type $suffix Optional. Set a suffix for the stylesheet.
+	 */
+	private function replace_css( $handle, $css_file, $suffix = null ) {
 		global $wp_styles;
-		$wp_styles->registered['colors-fresh']->src    = plugins_url( 'css/colors-mp6.css', __FILE__ );
-		$wp_styles->registered['colors-fresh']->ver    = filemtime( plugin_dir_path( __FILE__ ) . 'css/colors-mp6.css' );
-		$wp_styles->registered['colors-fresh']->deps[] = 'open-sans';
-		$wp_styles->registered['colors-fresh']->deps[] = 'dashicons';
-	}
-
-	public function replace_wp_default_styles() {
-		global $wp_styles;
-		$wp_styles->registered['buttons']->src                 = plugins_url( 'css/buttons.css', __FILE__ );
-		$wp_styles->registered['buttons']->ver                 = filemtime( plugin_dir_path( __FILE__ ) . 'css/buttons.css' );
-		$wp_styles->registered['editor-buttons']->src          = plugins_url( 'css/editor.css', __FILE__ );
-		$wp_styles->registered['editor-buttons']->ver          = filemtime( plugin_dir_path( __FILE__ ) . 'css/editor.css' );
-		$wp_styles->registered['media']->src                   = plugins_url( 'css/media.css', __FILE__ );
-		$wp_styles->registered['media']->ver                   = filemtime( plugin_dir_path( __FILE__ ) . 'css/media.css' );
-		$wp_styles->registered['media-views']->src             = plugins_url( 'css/media-views.css', __FILE__ );
-		$wp_styles->registered['media-views']->ver             = filemtime( plugin_dir_path( __FILE__ ) . 'css/media-views.css' );
-		$wp_styles->registered['media-views']->extra['suffix'] = '';
-		$wp_styles->registered['wp-admin']->src                = plugins_url( 'css/dark-admin.css', __FILE__ );
-		$wp_styles->registered['wp-admin']->ver                = filemtime( plugin_dir_path( __FILE__ ) . 'css/dark-admin.css' );
-		$wp_styles->registered['wp-admin']->deps[]             = 'open-sans';
-		$wp_styles->registered['wp-admin']->deps[]             = 'dashicons';
-		$wp_styles->registered['wp-admin']->extra['suffix']    = '';
-		$wp_styles->registered['wp-pointer']->src              = plugins_url( 'css/wp-pointer.css', __FILE__ );
-		$wp_styles->registered['wp-pointer']->ver              = filemtime( plugin_dir_path( __FILE__ ) . 'css/wp-pointer.css' );
+		$wp_styles = wp_styles();
+		$wp_styles->registered[ $handle ]->src = plugins_url( 'css/' . $css_file, __FILE__ );
+		$wp_styles->registered[ $handle ]->ver = filemtime( plugin_dir_path( __FILE__ ) . 'css/' . $css_file );
+		if ( ! is_null( $suffix ) ) {
+			$wp_styles->registered[ $handle ]->extra['suffix'] = $suffix;
+		}
 	}
 
 	public function replace_admin_bar_style() {
 		global $wp_styles;
+		$wp_styles = wp_styles();
 		if ( ! isset( $wp_styles->registered['admin-bar'] ) ) {
 			return;
 		}
-		$wp_styles->registered['admin-bar']->src             = plugins_url( 'css/dark-admin.css', __FILE__ );
-		$wp_styles->registered['admin-bar']->ver             = filemtime( plugin_dir_path( __FILE__ ) . 'css/dark-admin.css' );
-		$wp_styles->registered['admin-bar']->deps[]          = 'open-sans';
-		$wp_styles->registered['admin-bar']->deps[]          = 'dashicons';
-		$wp_styles->registered['admin-bar']->extra['suffix'] = '';
+		$this->replace_css( 'admin-bar', 'admin-bar.css', '' );
 	}
+
+	public function register_admin_color_scheme() {
+		global $_wp_admin_css_colors;
+		wp_admin_css_color(
+			'darkadmin',
+			__( 'DarkAdmin' ),
+			plugins_url( 'css/colors-darkadmin.css', __FILE__ ),
+			array( '#222', '#333', '#0074a2', '#2ea2cc' )
+		);
+		$_wp_admin_css_colors['darkadmin']->icon_colors = array( 'base' => '#999', 'focus' => '#2ea2cc', 'current' => '#fff' );
+		$this->replace_css( 'colors', 'colors-darkadmin.css' );
+	}
+
+	public function fix_login_color_scheme() {
+		$this->replace_css( 'colors-fresh', 'colors-darkadmin.css' );
+	}
+
+	public function replace_wp_default_styles() {
+		$this->replace_css( 'buttons',        'buttons.css' );
+		$this->replace_css( 'editor-buttons', 'editor.css' );
+		$this->replace_css( 'media',          'media.css' );
+		$this->replace_css( 'media-views',    'media-views.css', '' );
+		$this->replace_css( 'wp-admin',       'dark-admin.css', '' );
+		$this->replace_css( 'wp-pointer',     'wp-pointer.css' );
+	}
+
 
 	public function mce_init( $mce_init ) {
 		// make sure we don't override other custom `content_css` files
@@ -121,7 +114,7 @@ class DarkAdmin {
 
 	public function force_admin_color( $color_scheme ) {
 		global $_wp_admin_css_colors;
-		// if setting is `fresh`, `classic` or doesn't exist, change it to `mp6`
+		// if setting is `fresh`, `classic` or doesn't exist, change it to `darkadmin`
 		if ( ! isset( $_wp_admin_css_colors[ $color_scheme ] ) ) {
 				$color_scheme = 'darkadmin';
 		}
